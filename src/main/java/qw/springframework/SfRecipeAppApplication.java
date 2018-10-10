036dea3b01b63396df2b3336d3799b74c220e573
Qi Wu <qi.wu@elliemae.com>
2018-10-10 14:56:35 -0700

    Init

---
+++ src/main/java/qw/springframework/SfRecipeAppApplication.java
@@ -0,0 +1,12 @@
+package qw.springframework.sfrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfRecipeAppApplication.class, args);
+	}
+}
