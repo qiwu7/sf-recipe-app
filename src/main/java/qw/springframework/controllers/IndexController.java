77d9faa0820a051483a11d89528daf33c7f3f797
Qi Wu <qi.wu@elliemae.com>
2018-10-15 14:28:39 -0700

    Spring Data JPA Query method

--- src/main/java/qw/springframework/controllers/IndexController.java
+++ src/main/java/qw/springframework/controllers/IndexController.java
@@ -2,11 +2,32 @@ package qw.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import qw.springframework.domain.Category;
+import qw.springframework.domain.UnitOfMeasure;
+import qw.springframework.repositories.CategoryRepository;
+import qw.springframework.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        System.out.println(categoryOptional.get().getId());
+        System.out.println(unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
