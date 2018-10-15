2cbdd8f413d1965677167f8c4bbc53618f01ef50
Qi Wu <qi.wu@elliemae.com>
2018-10-15 14:58:28 -0700

    Display list of recipies

--- src/main/java/qw/springframework/controllers/IndexController.java
+++ src/main/java/qw/springframework/controllers/IndexController.java
@@ -1,32 +1,24 @@
 package qw.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import qw.springframework.domain.Category;
-import qw.springframework.domain.UnitOfMeasure;
-import qw.springframework.repositories.CategoryRepository;
-import qw.springframework.repositories.UnitOfMeasureRepository;
+import qw.springframework.services.RecipeService;
 
 import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage() {
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
-
-        System.out.println(categoryOptional.get().getId());
-        System.out.println(unitOfMeasureOptional.get().getId());
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
