aa2ae0115f99b1f00f0c441de64966be254d052d
Qi Wu <qi.wu@elliemae.com>
2018-10-17 10:12:33 -0700

    Category test

--- src/test/java/qw/springframework/sfrecipeapp/SfRecipeAppApplicationTests.java
+++ src/test/java/qw/springframework/sfrecipeapp/SfRecipeAppApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SfRecipeAppApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
