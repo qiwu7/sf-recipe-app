1707373ce22b14f13649b592262519bd64a28a7f
Qi Wu <qi.wu@elliemae.com>
2018-11-02 13:43:35 -0700

    Enhance 404 Page.

--- src/main/java/qw/springframework/services/RecipeServiceImpl.java
+++ src/main/java/qw/springframework/services/RecipeServiceImpl.java
@@ -41,7 +41,7 @@ public class RecipeServiceImpl implements RecipeService {
     public Recipe findById(Long id) {
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
         if (!recipeOptional.isPresent()) {
-            throw new NotFoundException("Recipe Not Found");
+            throw new NotFoundException("Recipe Not Found. For id value: " + id);
         }
         return recipeOptional.get();
     }
